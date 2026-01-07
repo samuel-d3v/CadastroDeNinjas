3d555964ee74b2a53043bf121f8c883d54779f5f
samuel-d3v <samuel_o.assis@hotmail.com>
2026-01-07 17:25:41 -0300

    feat: add NinjaEntity JPA mapping (close #2)

---
+++ src/main/java/br/com/samueloliveira/CadastroDeNinjas/model/entity/NinjaEntity.java
@@ -0,0 +1,40 @@
+package br.com.samueloliveira.CadastroDeNinjas.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro_ninjas")
+public class NinjaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nome;
+    private int idade;
+    private String email;
+
+    public NinjaEntity(){
+    }
+
+    public NinjaEntity(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "NinjaEntity [nome=" + nome + ", idade=" + idade + ", email=" + email + "]";
+    }
+}
