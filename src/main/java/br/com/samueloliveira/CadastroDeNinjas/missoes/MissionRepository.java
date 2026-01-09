fae745bf6f18fae6378098160ab4100c91830f9e
samuel-d3v <samuel_o.assis@hotmail.com>
2026-01-09 18:26:20 -0300

    feat: add MissionRepository extending JpaRepository

---
+++ src/main/java/br/com/samueloliveira/CadastroDeNinjas/missoes/MissionRepository.java
@@ -0,0 +1,7 @@
+package br.com.samueloliveira.CadastroDeNinjas.missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionRepository extends JpaRepository<MissionModel, Long> {
+
+}
