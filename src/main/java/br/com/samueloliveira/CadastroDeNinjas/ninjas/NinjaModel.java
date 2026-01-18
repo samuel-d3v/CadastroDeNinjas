082120e4fb9f9e06a1c4a17279dab1b078185fd0
samuel-d3v <samuel_o.assis@hotmail.com>
2026-01-17 21:09:22 -0300

    feat(ninjas): add level attribute and validations

--- src/main/java/br/com/samueloliveira/CadastroDeNinjas/ninjas/NinjaModel.java
+++ src/main/java/br/com/samueloliveira/CadastroDeNinjas/ninjas/NinjaModel.java
@@ -2,6 +2,8 @@ package br.com.samueloliveira.CadastroDeNinjas.ninjas;
 
 import br.com.samueloliveira.CadastroDeNinjas.missoes.MissionModel;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
@@ -12,16 +14,21 @@ import lombok.*;
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Setter(AccessLevel.NONE)
     private Long id;
 
+    @NotBlank (message = "O nome é obrigatório.")
     private String nome;
     private int idade;
 
     @Column(unique = true)
+    @NotBlank (message = "O e-mail é obrigatório.")
+    @Email (message = "O formato do email é inválido.")
     private String email;
+
     @ManyToOne
     @JoinColumn(name = "missao_id")
     private MissionModel missao;
 
+    @Column (name = "level")
+    private String level;
 }
